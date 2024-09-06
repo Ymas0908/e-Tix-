182364cd279ff920d31f29110827bd15be9b6176
samuel.kebe <samuel.kebe@it-centrex.com>
2024-09-06 08:23:55 +0000

    "update 06/09/24 "

---
+++ lib/pages/pageacceuil.dart
@@ -0,0 +1,150 @@
+import 'package:auth_firebase/pages/login/login.dart';
+import 'package:auth_firebase/pages/signup/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Pageacceuil extends StatelessWidget {
+  const Pageacceuil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+               const SizedBox(
+                height: 20,
+              ),
+              SvgPicture.asset(
+                'assets/login.svg',
+                height: 300,
+                width: 300,
+              ),
+              Center(
+                child: Text(
+                  'Bienvenue sur Auth App',
+                  style: GoogleFonts.raleway(
+                      textStyle: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25)),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Authentifiez-vous pour continuer",
+                style: GoogleFonts.raleway(
+                    textStyle: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 16)),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              _signin(context),
+              const SizedBox(
+                height: 50,
+              ),
+              _signup(context)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _signin(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xff0D6EFD),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+        minimumSize: const Size(double.infinity, 60),
+        elevation: 0,
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => const Login(),
+          ),
+        );
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.login,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(
+            "Connexion",
+            style: GoogleFonts.raleway(
+              textStyle: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _signup(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xff0D6EFD),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+        minimumSize: const Size(double.infinity, 60),
+        elevation: 0,
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => const Signup(),
+          ),
+        );
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.login,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(
+            "Inscription",
+            style: GoogleFonts.raleway(
+              textStyle: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
