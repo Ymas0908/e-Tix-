c32a5642a9290637d3c345b839aef3a6b64fb096
samuel.kebe <samuel.kebe@it-centrex.com>
2024-09-06 08:24:18 +0000

    "update 06/09/24 "

---
+++ lib/composants/LoadingDialog.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoadingDialog extends StatelessWidget {
+  final String message;
+
+  const LoadingDialog({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const CircularProgressIndicator(),
+            const SizedBox(height: 20),
+            Text(
+              message,
+              style: const TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
